ad6ae2e1a4685f21ebfaa51caa90b458c4ac80e8
Thomas Zemp <tzemp@baosystems.com>
2019-08-29 12:12:32 +0200

    #11: add file to load initial blank database

---
+++ database/createDBtables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE maps(
+	id SERIAL PRIMARY KEY,
+	name varchar(100) UNIQUE,
+	created timestamp,
+	updated timestamp,
+	uid varchar(6),
+	questionnaireUID varchar(100),
+	map json
+);
+
+CREATE TABLE questionnaires(
+	id SERIAL PRIMARY KEY,
+	name varchar(100) UNIQUE,
+	created timestamp,
+	updated timestamp,
+	uid varchar(6),
+	questionnaire json
+);
