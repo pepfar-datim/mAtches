544316d4ea9944a93079c96f226eafa1403614dd
Thomas Zemp <tzemp@baosystems.com>
2019-09-06 17:31:06 +0200

    #21 #22 format questionnaire/map in sidebar

--- src/MapUpload.js
+++ src/MapUpload.js
@@ -2,24 +2,15 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
-import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import IconButton from "@material-ui/core/IconButton";
+import Edit from '@material-ui/icons/Edit';
 
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-
 import { sizing } from '@material-ui/system';
 
 import UploadCard from "./UploadCard.js";
+import config from '../config.json'
 
 const drawerWidth = 200;
 
@@ -36,11 +27,29 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function formatQuestions(mapCheck) {
+  return Object.keys(mapCheck['flatQuestionnaire']).map(function (k, i) {
+    return(
+      <div key={'question-'+i}>
+        <Typography wrap="noWrap">
+          <strong>{mapCheck['flatQuestionnaire'][k]['text']}</strong>
+        </Typography>
+        <Typography wrap="noWrap">
+          {mapCheck['flatQuestionnaire'][k]['header'] || ''}
+        </Typography>
+        <br />
+      </div>  
+    )
+  })  
+}
+
+
 function MapUpload(props) {
   const classes = useStyles();
 
   var map = props.map;  
-  var mapCheck = props.mapCheck;  
+  var mapCheck = props.mapCheck;
+
 
   return (
     <div style={{"padding": "20px"}}>
@@ -54,24 +63,21 @@ function MapUpload(props) {
                 <Typography variant="body1">
                   ({props.questionnaireName})
                 </Typography>                
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick = {() => {window.location = config.base + 'maps/' + map.uid + "?mode=edit"}}
+                >
+                  <Edit />
+                </IconButton>
+                <br />
                 <br />
                 {props.mapCheck &&
                 <div>
-                  <Typography wrap="noWrap">
-                    <strong>{mapCheck['flatQuestionnaire']['/Patient/id']['text']}</strong>
-                  </Typography>
-                  <Typography wrap="noWrap">
-                    {mapCheck['flatQuestionnaire']['/Patient/id']['header']}
-                  </Typography>                  
+                  {formatQuestions(props.mapCheck)}
                 </div>
-                }
-                <Typography>
-                  <br/>
-                  Question 1<br/>
-                  Column A<br /><br/>
-                  Question 2<br/>
-                  Column B<br /><br/>                
-                </Typography>      
+                }  
               </div>        
           </Card>
       </Grid>
