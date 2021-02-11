bc39252ece9ed36a5eb0377ed19cf01f911d67c3
Thomas Zemp <tzemp@baosystems.com>
2021-02-11 12:10:30 +0100

    clean up of server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const app = express();
 const api = require("./queries");
-const port = 5001;
+const config = require("../config.json");
+
+const app = express();
+
+const { port, base: basePath } = config;
 
 const DIST_DIR = path.join(__dirname, "../dist");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
-const ERROR_FILE = path.join(DIST_DIR, "error.html");
+const HTML_FILE = path.join(DIST_DIR, basePath, "index.html");
+const ERROR_FILE = path.join(DIST_DIR, basePath, "error.html");
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.text({ limit: "50mb" }));
 app.use(
@@ -20,47 +24,44 @@ app.use(
 
 app.use(express.static(DIST_DIR));
 
-const config = require("../config.json");
-const basePath = config.base;
+app.get(`${basePath}api/about`, api.getAbout);
 
-app.get(basePath + "api/about", api.getAbout);
+app.get(`${basePath}api/fileTypes`, api.getFileTypes);
 
-app.get(basePath + "api/fileTypes", api.getFileTypes);
+app.get(`${basePath}api/maps`, api.getAll);
 
-app.get(basePath + "api/maps", api.getAll);
+app.get(`${basePath}api/maps/names/:name`, api.checkName);
 
-app.get(basePath + "api/maps/names/:name", api.checkName);
+app.get(`${basePath}api/maps/:id`, api.getSpecificResource);
 
-app.get(basePath + "api/maps/:id", api.getSpecificResource);
+app.post(`${basePath}api/maps`, api.createMap);
 
-app.post(basePath + "api/maps", api.createMap);
+app.put(`${basePath}api/maps`, api.updateMap);
 
-app.put(basePath + "api/maps", api.updateMap);
-
-app.post(basePath + "api/maps/:id/upload", api.uploadData);
+app.post(`${basePath}api/maps/:id/upload`, api.uploadData);
 
 app.post(
-  basePath + "api/maps/:id/upload/valueMap/:header",
+  `${basePath}api/maps/:id/upload/valueMap/:header`,
   api.validateValueMapPayload
 );
 
-app.delete(basePath + "api/maps/:id", api.deleteSpecificResource);
+app.delete(`${basePath}api/maps/:id`, api.deleteSpecificResource);
 
-app.get(basePath + "api/questionnaires", api.getFHIRQuestionnaires);
+app.get(`${basePath}api/questionnaires`, api.getFHIRQuestionnaires);
 
-app.get(basePath + "api/questionnaires/:id", api.getSpecificQuestionnaire);
+app.get(`${basePath}api/questionnaires/:id`, api.getSpecificQuestionnaire);
 
-app.delete(basePath + "api/questionnaires/:id", api.deleteSpecificResource);
+app.delete(`${basePath}api/questionnaires/:id`, api.deleteSpecificResource);
 
 app.get(basePath, (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
-app.get(basePath + "maps", (req, res) => {
+app.get(`${basePath}maps`, (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
-app.get(basePath + "maps/:id", (req, res) => {
+app.get(`${basePath}maps/:id`, (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
